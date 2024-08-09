3acdb63eb085f146e878a52b1f1308487c840bf7
Rehno Lindeque <rehno.lindeque@gmail.com>
2024-08-09 16:58:59 +0000

    computeCircumcircle: minor refactor

--- delaunay-editor.js
+++ delaunay-editor.js
@@ -109,7 +109,7 @@ class DelaunayTriangle {
 
     const aux1 = (dA * (p3.y - p2.y) + dB * (p1.y - p3.y) + dC * (p2.y - p1.y));
     const aux2 = -(dA * (p3.x - p2.x) + dB * (p1.x - p3.x) + dC * (p2.x - p1.x));
-    const div = (2 * (p1.x * (p3.y - p2.y) + p2.x * (p1.y - p3.y) + p3.x * (p2.y - p1.y)));
+    const div = 2 * (p1.x * (p3.y - p2.y) + p2.x * (p1.y - p3.y) + p3.x * (p2.y - p1.y));
 
     const center = new Point(aux1 / div, aux2 / div);
     const radius = Math.sqrt((center.x - p1.x) ** 2 + (center.y - p1.y) ** 2);
