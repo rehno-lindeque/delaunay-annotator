e6e28f73020a04aeb86bce21d4385d742314ddf9
Rehno Lindeque <rehno.lindeque@gmail.com>
2024-08-14 19:19:49 +0000

    add a nix flake

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Some web components for segmentations";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
+
+    supportedSystems = lib.platforms.all;
+    developerSystems = ["x86_64-linux" "aarch64-linux"];
+
+    legacyPackages = lib.genAttrs supportedSystems (system: import nixpkgs {inherit system;});
+  in {
+    checks = self.packages;
+
+    formatter = lib.genAttrs developerSystems (system: legacyPackages.${system}.alejandra);
+
+    packages = lib.genAttrs supportedSystems (
+      system: {
+        default = self.packages.${system}.dist;
+
+        dist = legacyPackages.${system}.stdenvNoCC.mkDerivation {
+          pname = "segmentation-components-dist";
+          version = "1.0.0";
+
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.fileFilter (file: file.hasExt "html" || file.hasExt "js") ./.;
+          };
+
+          installPhase = ''
+            mkdir -p $out
+            cp -r * $out/
+          '';
+
+          meta = with lib; {
+            description = "Segmentation annotation tool as a web component";
+            license = licenses.mit;
+            platforms = platforms.all;
+          };
+        };
+      }
+    );
+  };
+}
